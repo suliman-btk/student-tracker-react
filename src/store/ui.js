86ce524481b41565fd63e19efa9937645ba9ea2d
suliman-btk <suliman56563@gmail.com>
2026-07-06 18:47:43 +0800

    Refactor UI store to use Zustand's persist middleware

--- src/store/ui.js
+++ src/store/ui.js
@@ -1,11 +1,20 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useUI = create((set) => ({
-  aiOpen: false,
-  aiContext: null,
-  activeSpaceId: null,
-  toggleAI: (ctx) => set((s) => ({ aiOpen: !s.aiOpen, aiContext: ctx ?? s.aiContext })),
-  openAI: (ctx) => set({ aiOpen: true, aiContext: ctx }),
-  closeAI: () => set({ aiOpen: false }),
-  setActiveSpace: (id) => set({ activeSpaceId: id }),
-}));
+export const useUI = create(
+  persist(
+    (set) => ({
+      aiOpen: false,
+      aiContext: null,
+      activeSpaceId: null,
+      toggleAI: (ctx) => set((s) => ({ aiOpen: !s.aiOpen, aiContext: ctx ?? s.aiContext })),
+      openAI: (ctx) => set({ aiOpen: true, aiContext: ctx }),
+      closeAI: () => set({ aiOpen: false }),
+      setActiveSpace: (id) => set({ activeSpaceId: id }),
+    }),
+    {
+      name: "raqip-ui",
+      partialize: (s) => ({ activeSpaceId: s.activeSpaceId }),
+    }
+  )
+);
